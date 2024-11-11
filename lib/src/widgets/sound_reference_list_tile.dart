813296b2723bf38896283fd5c758223c8c699559
Chris Norman <chris.norman1989@outlook.com>
2024-11-11 14:43:32 +0000

    Allow changing sound gains and deleting from keyboard and screen.

--- lib/src/widgets/sound_reference_list_tile.dart
+++ lib/src/widgets/sound_reference_list_tile.dart
@@ -1,9 +1,11 @@
 import 'package:backstreets_widgets/extensions.dart';
+import 'package:backstreets_widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_audio_games/flutter_audio_games.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path/path.dart' as path;
 
+import '../../level_editor.dart';
 import '../json/sound_reference.dart';
 import '../providers.dart';
 import '../screens/select_sound_screen.dart';
@@ -69,24 +71,91 @@ class SoundReferenceListTile extends ConsumerWidget {
     return PlaySoundSemantics(
       sound: sound,
       child: Builder(
-        builder: (final builderContext) => ListTile(
-          autofocus: autofocus,
-          title: Text(title),
-          subtitle: Text(path.basename(reference.path)),
-          onTap: () => builderContext.pushWidgetBuilder((final innerContext) {
-            builderContext.stopPlaySoundSemantics();
-            return SelectSoundScreen(
-              paths: possiblePaths,
-              currentPath: reference.path,
-              onDone: (final newPath) {
-                reference.path = newPath;
-                onChanged(reference);
-              },
-              looping: looping,
-            );
-          }),
-        ),
+        builder: (final builderContext) {
+          final subtitle = path.basename(reference.path);
+          final gain = reference.volume.toStringAsFixed(2);
+          return CommonShortcuts(
+            deleteCallback: () => onChanged(null),
+            moveDownCallback: () => adjustGain(builderContext, -0.1),
+            moveUpCallback: () => adjustGain(builderContext, 0.1),
+            child: ListTile(
+              autofocus: autofocus,
+              title: Text(title),
+              subtitle: Semantics(
+                label: '$subtitle ($gain)',
+                child: ExcludeSemantics(child: Text(subtitle)),
+              ),
+              onTap: () => builderContext.pushWidgetBuilder(
+                (final innerContext) {
+                  builderContext.stopPlaySoundSemantics();
+                  return SelectSoundScreen(
+                    paths: possiblePaths,
+                    currentPath: reference.path,
+                    onDone: (final newPath) {
+                      reference.path = newPath;
+                      onChanged(reference);
+                    },
+                    looping: looping,
+                  );
+                },
+              ),
+              trailing: FocusScope(
+                canRequestFocus: false,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    if (reference.volume < maxVolume)
+                      Expanded(
+                        flex: 2,
+                        child: IconButton(
+                          onPressed: () => adjustGain(builderContext, 0.1),
+                          icon: const Icon(Icons.volume_up),
+                          tooltip: 'Volume up',
+                        ),
+                      ),
+                    Text(gain),
+                    if (reference.volume > minVolume)
+                      Expanded(
+                        flex: 2,
+                        child: IconButton(
+                          onPressed: () => adjustGain(builderContext, -0.1),
+                          icon: const Icon(Icons.volume_down),
+                          tooltip: 'Volume down',
+                        ),
+                      ),
+                    Expanded(
+                      child: IconButton(
+                        onPressed: () => onChanged(null),
+                        icon: const Icon(Icons.delete),
+                        tooltip: 'Delete',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
+  }
+
+  /// Adjust the gain of [soundReference] by [amount].
+  void adjustGain(final BuildContext buildContext, final double amount) {
+    final reference = soundReference!;
+    setGain(buildContext, reference.volume + amount);
+  }
+
+  /// Set the volume of [soundReference] to [volume].
+  void setGain(final BuildContext buildContext, final double volume) {
+    final reference = soundReference!
+      ..volume = volume.clamp(minVolume, maxVolume);
+    onChanged(soundReference);
+    buildContext
+        .findAncestorStateOfType<PlaySoundSemanticsState>()
+        ?.handle
+        ?.volume
+        .value = reference.volume;
   }
 }
