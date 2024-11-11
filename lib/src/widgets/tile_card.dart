6eede7dd4122b9dcf3b7c3d4bacfcb80c3d426a1
Chris Norman <chris.norman1989@outlook.com>
2024-11-11 10:58:32 +0000

    Add linking menu.

--- lib/src/widgets/tile_card.dart
+++ lib/src/widgets/tile_card.dart
@@ -64,215 +64,301 @@ class TileCard extends ConsumerWidget {
         id == null ? null : ref.watch(platformProvider(levelId, id));
     final level = ref.watch(gameLevelProvider(levelId));
     final editor = ref.watch(levelEditorContextProvider);
-    final Sound sound;
-    final List<PerformableAction> actions;
-    final GameLevelTerrainReference? terrain;
-    if (platform == null) {
-      terrain = null;
-      sound = editor.wallSound;
-      actions = [
-        PerformableAction(
-          name: 'New platform',
-          activator: SingleActivator(
-            LogicalKeyboardKey.keyN,
-            control: useControlKey,
-            meta: useMetaKey,
+    final terrain = platform == null
+        ? null
+        : ref.watch(terrainProvider(platform.terrainId));
+    final menuChildren = <Widget>[];
+    if (platform != null) {
+      final link = platform.link;
+      if (link != null) {
+        menuChildren.addAll([
+          MenuItemButton(
+            autofocus: linkingPlatformId == null,
+            child: Text(link.move ? 'Unlink move' : 'Link move'),
+            onPressed: () {
+              final oldValue = link.move;
+              final action = UndoableAction(
+                perform: () => link.move = !oldValue,
+                undo: () => link.move = oldValue,
+              );
+              performAction(action);
+            },
           ),
-          invoke: () {
-            final terrains = ref.read(terrainsProvider);
-            final platform = GameLevelPlatformReference(
-              id: newId(),
-              terrainId: terrains.first.id,
-              startX: max(0, coordinates.x),
-              startY: max(0, coordinates.y),
-            );
-            if (coordinates.x < 0 || coordinates.y < 0) {
-              for (final other in level.platforms) {
-                if (coordinates.x < 0) {
-                  other.startX -= coordinates.x;
-                }
-                if (coordinates.y < 0) {
-                  other.startY -= coordinates.y;
-                }
-              }
-            }
-            final action = UndoableAction(
-              perform: () {
-                level.platforms.add(platform);
-              },
-              undo: () {
-                level.platforms
-                    .removeWhere((final other) => other.id == platform.id);
-              },
-            );
-            performAction(action);
-          },
-        ),
-      ];
-    } else {
-      terrain = ref.watch(terrainProvider(platform.terrainId));
-      final footstepSounds = ref.watch(
-        footstepsProvider(
-          key: terrain!.footstepSounds,
-          destroy: true,
-          volume: terrain.footstepSoundsGain,
-        ),
-      );
-      sound = footstepSounds.randomElement(random);
-      actions = [
-        PerformableAction(
-          name: 'Rename',
-          activator: SingleActivator(
-            LogicalKeyboardKey.keyR,
-            control: useControlKey,
-            meta: useMetaKey,
+          MenuItemButton(
+            child: Text(link.resize ? 'Unlink resize' : 'Link resize'),
+            onPressed: () {
+              final oldValue = link.resize;
+              final action = UndoableAction(
+                perform: () => link.resize = !oldValue,
+                undo: () => link.resize = oldValue,
+              );
+              performAction(action);
+            },
           ),
-          invoke: () {
-            final oldName = platform.name;
-            context.pushWidgetBuilder(
-              (final getTextContext) => GetText(
-                onDone: (final value) {
-                  Navigator.pop(getTextContext);
-                  final action = UndoableAction(
-                    perform: () => platform.name = value,
-                    undo: () => platform.name = oldName,
-                  );
-                  performAction(action);
-                },
-                labelText: 'Platform name',
-                text: oldName,
-                title: 'Rename Platform',
+          MenuItemButton(
+            child: const Text('Delete link'),
+            onPressed: () {
+              final action = UndoableAction(
+                perform: () => platform.link = null,
+                undo: () => platform.link = link,
+              );
+              performAction(action);
+            },
+          ),
+        ]);
+      } else if (linkingPlatformId == null) {
+        menuChildren.add(
+          MenuItemButton(
+            autofocus: true,
+            onPressed: linkPlatforms,
+            child: const Text('Start linking'),
+          ),
+        );
+      }
+      if (linkingPlatformId == platform.id) {
+        menuChildren.add(
+          MenuItemButton(
+            autofocus: true,
+            onPressed: linkPlatforms,
+            child: const Text('Cancel link'),
+          ),
+        );
+      } else if (linkingPlatformId != null) {
+        menuChildren.add(
+          MenuItemButton(
+            autofocus: true,
+            onPressed: linkPlatforms,
+            child: const Text('Complete link'),
+          ),
+        );
+      }
+    }
+    return MenuAnchor(
+      menuChildren: menuChildren,
+      builder: (final builderContext, final controller, final child) {
+        final Sound sound;
+        final List<PerformableAction> actions;
+        if (platform == null) {
+          sound = editor.wallSound;
+          actions = [
+            PerformableAction(
+              name: 'New platform',
+              activator: SingleActivator(
+                LogicalKeyboardKey.keyN,
+                control: useControlKey,
+                meta: useMetaKey,
               ),
-            );
-          },
-        ),
-        PerformableAction(
-          name: 'Change terrain',
-          activator: SingleActivator(
-            LogicalKeyboardKey.keyT,
-            control: useControlKey,
-            meta: useMetaKey,
-          ),
-          invoke: () {
-            final oldTerrainId = platform.terrainId;
-            context.pushWidgetBuilder(
-              (final _) => SelectTerrainScreen(
-                terrainId: platform.terrainId,
-                onChanged: (final value) {
-                  final action = UndoableAction(
-                    perform: () {
-                      platform.terrainId = value.id;
-                    },
-                    undo: () {
-                      platform.terrainId = oldTerrainId;
-                    },
-                  );
-                  performAction(action);
-                },
-              ),
-            );
-          },
-        ),
-        PerformableAction(
-          name: 'Delete',
-          activator: const SingleActivator(LogicalKeyboardKey.delete),
-          invoke: () {
-            for (final other in level.platforms) {
-              if (other.link?.platformId == platform.id) {
-                context.showMessage(
-                  message:
-                      // ignore: lines_longer_than_80_chars
-                      'You cannot delete the ${platform.name} platform until you have unlinked it from ${other.name}.',
+              invoke: () {
+                final terrains = ref.read(terrainsProvider);
+                final platform = GameLevelPlatformReference(
+                  id: newId(),
+                  terrainId: terrains.first.id,
+                  startX: max(0, coordinates.x),
+                  startY: max(0, coordinates.y),
                 );
-                return;
-              }
-            }
-            context.confirm(
-              message:
-                  // ignore: lines_longer_than_80_chars
-                  'Are you sure you want to delete the ${platform.name} platform?',
-              title: 'Confirm Delete',
-              yesCallback: () {
-                Navigator.pop(context);
+                if (coordinates.x < 0 || coordinates.y < 0) {
+                  for (final other in level.platforms) {
+                    if (coordinates.x < 0) {
+                      other.startX -= coordinates.x;
+                    }
+                    if (coordinates.y < 0) {
+                      other.startY -= coordinates.y;
+                    }
+                  }
+                }
                 final action = UndoableAction(
                   perform: () {
-                    level.platforms.removeWhere(
-                      (final other) => other.id == platform.id,
-                    );
+                    level.platforms.add(platform);
                   },
                   undo: () {
-                    level.platforms.add(platform);
+                    level.platforms
+                        .removeWhere((final other) => other.id == platform.id);
                   },
                 );
                 performAction(action);
               },
-            );
-          },
-        ),
-      ];
-    }
-    return PlaySoundSemantics(
-      key: platform == null ? null : ValueKey(platform.toJson().toString()),
-      sound: sound,
-      child: PerformableActions(
-        actions: actions,
-        child: FocusableActionDetector(
-          autofocus: autofocus,
-          actions: {
-            ActivateIntent: CallbackAction(
-              onInvoke: (final intent) {
-                if (platform == null) {
-                  return null;
-                }
+            ),
+          ];
+        } else {
+          final footstepSounds = ref.watch(
+            footstepsProvider(
+              key: terrain!.footstepSounds,
+              destroy: true,
+              volume: terrain.footstepSoundsGain,
+            ),
+          );
+          sound = footstepSounds.randomElement(random);
+          actions = [
+            PerformableAction(
+              name: 'Rename',
+              activator: SingleActivator(
+                LogicalKeyboardKey.keyR,
+                control: useControlKey,
+                meta: useMetaKey,
+              ),
+              invoke: () {
+                final oldName = platform.name;
                 context.pushWidgetBuilder(
-                  (final _) => EditPlatformScreen(
-                    platformId: platform.id,
-                    levelId: levelId,
+                  (final getTextContext) => GetText(
+                    onDone: (final value) {
+                      Navigator.pop(getTextContext);
+                      final action = UndoableAction(
+                        perform: () => platform.name = value,
+                        undo: () => platform.name = oldName,
+                      );
+                      performAction(action);
+                    },
+                    labelText: 'Platform name',
+                    text: oldName,
+                    title: 'Rename Platform',
                   ),
                 );
-                return null;
               },
             ),
-          },
-          child: Semantics(
-            label:
-                // ignore: lines_longer_than_80_chars
-                '${coordinates.x}, ${coordinates.y}: ${platform == null ? "Wall" : '${platform.name} (${terrain!.name})'}',
-            child: Card(
-              color: platform == null ? Colors.grey.shade300 : Colors.white,
-              elevation: 3,
-              margin: const EdgeInsets.all(8),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
+            PerformableAction(
+              name: 'Change terrain',
+              activator: SingleActivator(
+                LogicalKeyboardKey.keyT,
+                control: useControlKey,
+                meta: useMetaKey,
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '${coordinates.x}, ${coordinates.y}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
+              invoke: () {
+                final oldTerrainId = platform.terrainId;
+                context.pushWidgetBuilder(
+                  (final _) => SelectTerrainScreen(
+                    terrainId: platform.terrainId,
+                    onChanged: (final value) {
+                      final action = UndoableAction(
+                        perform: () {
+                          platform.terrainId = value.id;
+                        },
+                        undo: () {
+                          platform.terrainId = oldTerrainId;
+                        },
+                      );
+                      performAction(action);
+                    },
+                  ),
+                );
+              },
+            ),
+            PerformableAction(
+              name: 'Link platforms',
+              activator: SingleActivator(
+                LogicalKeyboardKey.keyL,
+                control: useControlKey,
+                meta: useMetaKey,
+              ),
+              invoke: () {
+                if (controller.isOpen) {
+                  controller.close();
+                } else {
+                  controller.open();
+                }
+              },
+            ),
+            PerformableAction(
+              name: 'Delete',
+              activator: const SingleActivator(LogicalKeyboardKey.delete),
+              invoke: () {
+                for (final other in level.platforms) {
+                  if (other.link?.platformId == platform.id) {
+                    context.showMessage(
+                      message:
+                          // ignore: lines_longer_than_80_chars
+                          'You cannot delete the ${platform.name} platform until you have unlinked it from ${other.name}.',
+                    );
+                    return;
+                  }
+                }
+                context.confirm(
+                  message:
+                      // ignore: lines_longer_than_80_chars
+                      'Are you sure you want to delete the ${platform.name} platform?',
+                  title: 'Confirm Delete',
+                  yesCallback: () {
+                    Navigator.pop(context);
+                    final action = UndoableAction(
+                      perform: () {
+                        level.platforms.removeWhere(
+                          (final other) => other.id == platform.id,
+                        );
+                      },
+                      undo: () {
+                        level.platforms.add(platform);
+                      },
+                    );
+                    performAction(action);
+                  },
+                );
+              },
+            ),
+          ];
+        }
+        return PlaySoundSemantics(
+          key: platform == null ? null : ValueKey(platform.toJson().toString()),
+          sound: sound,
+          child: PerformableActions(
+            actions: actions,
+            child: FocusableActionDetector(
+              autofocus: autofocus,
+              actions: {
+                ActivateIntent: CallbackAction(
+                  onInvoke: (final intent) {
+                    if (platform == null) {
+                      return null;
+                    }
+                    context.pushWidgetBuilder(
+                      (final _) => EditPlatformScreen(
+                        platformId: platform.id,
+                        levelId: levelId,
                       ),
+                    );
+                    return null;
+                  },
+                ),
+              },
+              child: Semantics(
+                label:
+                    // ignore: lines_longer_than_80_chars
+                    '${coordinates.x}, ${coordinates.y}: ${platform == null ? "Wall" : '${platform.name} (${terrain!.name})'}',
+                child: Card(
+                  color: platform == null ? Colors.grey.shade300 : Colors.white,
+                  elevation: 3,
+                  margin: const EdgeInsets.all(8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '${coordinates.x}, ${coordinates.y}',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: platform == null
+                              ? const Icon(
+                                  Icons.crop_din,
+                                  semanticLabel: 'Wall',
+                                )
+                              : Text('${platform.name} (${terrain!.name})'),
+                        ),
+                      ],
                     ),
-                    Expanded(
-                      flex: 3,
-                      child: platform == null
-                          ? const Icon(
-                              Icons.crop_din,
-                              semanticLabel: 'Wall',
-                            )
-                          : Text('${platform.name} (${terrain!.name})'),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
